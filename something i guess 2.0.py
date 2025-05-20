a7ef1d88c432fb8b50a561573c2e3d5198bec628
glitchfan <luke.searancke@kingsway.school.nz>
2025-05-20 18:12:11 +1200

    Add files via upload

---
+++ something i guess 2.0.py
@@ -0,0 +1,9 @@
+num1 = int(input("what is the width?"))
+num2 = int(input("what is the hight"))
+print(f"you entered the width as {num1}")
+print(f"you entered the hight as {num2}")
+
+sum = num1 * num2
+sum2 = (num1 * 2) + (num2 * 2)
+print(f"your area is {sum}")
+print(f"your perimeter is {sum2}")
