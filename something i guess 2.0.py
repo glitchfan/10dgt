f82b2cdd84a5453226cd19fda5d25ce05f8f4703
glitchfan <luke.searancke@kingsway.school.nz>
2025-05-23 14:12:33 +1200

    Add files via upload
    erea coculater 
    im bad at spelling

--- something i guess 2.0.py
+++ something i guess 2.0.py
@@ -1,9 +1,37 @@
-num1 = int(input("what is the width?"))
-num2 = int(input("what is the hight"))
-print(f"you entered the width as {num1}")
-print(f"you entered the hight as {num2}")
+# from turtle import width
+def num_check():
+        keep_going = ""
+        while keep_going == "":
+            while True:
+                try:
+                    width = float(input("width: "))
+                    if width > 0:  # You can add additional validation criteria here
+                        break
+                    else:
+                        print("Please enter a positive number.")
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
 
-sum = num1 * num2
-sum2 = (num1 * 2) + (num2 * 2)
-print(f"your area is {sum}")
-print(f"your perimeter is {sum2}")
+            while True:
+                try:
+                    height = float(input("height: "))
+                    if height > 0:  # You can add additional validation criteria here
+                        break
+                    else:
+                        print("Please enter a positive number.")
+                except ValueError:
+                    print("Invalid input. Please enter a valid integer.")
+
+
+            sum = round((width * height), 2)
+            sum2 = round(((width * 2) + (height * 2)), 2)
+            print()
+            print(f"your area is {sum} units")
+            print(f"your perimeter is {sum2} square units")
+                                
+            keep_going = input("press enter to keep going or any key to quit") 
+            print()
+            
+            print("thank you for useing my program insted of the other dumb ones")
+
+num_check()
